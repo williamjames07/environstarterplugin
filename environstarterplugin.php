cdd2fed53914d0cb6b0a349cec3debe57da196e9
William McIntyre <williamjames.developer@gmail.com>
2018-12-11 23:11:56 -0800

    Silence is golden

--- environstarterplugin.php
+++ environstarterplugin.php
@@ -3,11 +3,11 @@
  * ---------------------------------------------------------------------
  * License & Details
  * ---------------------------------------------------------------------
- * PLugin Name: 	Environ
- * Plugin URI: 		https://environ.io
- * Description: 	Environ Starter is an easy to use framework for modern day WordPress plugin development.
- * Author: 			William McIntyre
- * Author URI: 		https://environ.io
- * Version: 		1.0.0
- * License: 		MIT
+ * PLugin Name:     Environ
+ * Plugin URI:      https://environ.io
+ * Description:     Environ Starter is an easy to use framework for modern day WordPress plugin development.
+ * Author:          William McIntyre
+ * Author URI:      https://environ.io
+ * Version:         1.0.0
+ * License:         MIT
  */
