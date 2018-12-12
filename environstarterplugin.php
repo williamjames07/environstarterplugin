da3451eb5fba0a676d0692d610b343e8a47e7fa2
William McIntyre <williamjames.developer@gmail.com>
2018-12-11 23:01:16 -0800

    Environment setup and plugin details

---
+++ environstarterplugin.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * ---------------------------------------------------------------------
+ * License & Details
+ * ---------------------------------------------------------------------
+ * PLugin Name: 	Environ
+ * Plugin URI: 		https://environ.io
+ * Description: 	Environ Starter is an easy to use framework for modern day WordPress plugin development.
+ * Author: 			William McIntyre
+ * Author URI: 		https://environ.io
+ * Version: 		1.0.0
+ * License: 		MIT
+ */
